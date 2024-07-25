e9c573e57923b02a7d901facfad1fabd21e559fd
Raghav Raj Sah <raghavrajsah@gmail.com>
2024-07-24 18:37:48 -0700

    Client removed

--- LLClient.java
+++ LLClient.java
@@ -2,11 +2,6 @@ public class LLClient {
 
     public static void main(String[] args) {
 
-        LinkedList<String> raghav = new LinkedList<>();
-        raghav.add("cat");
-        raghav.add("dog");
-        raghav.add("parrot");
-        System.out.println(raghav.toString());
         
     }
     
